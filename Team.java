f1e48ddb1d5dcf76556edec9191dbdece701ea29
arexxd <60717897+arexxd@users.noreply.github.com>
2022-03-01 15:26:40 -0600

    Update teams

---
+++ Team.java
@@ -0,0 +1,19 @@
+abstract class Team {
+    private String name;
+    private int points;
+    public Team(String pname, int ppoints)
+        {
+        name = pname;
+        points = ppoints;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public int getPoints()
+        {
+            return points;
+        }
+        
+
+    }
