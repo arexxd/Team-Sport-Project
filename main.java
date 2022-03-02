036f471f5a44febd8ceba369716f1409c1c87537
arexxd <60717897+arexxd@users.noreply.github.com>
2022-03-02 15:21:44 -0600

    Updates and deletes

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+    
+
+
+
+
+
+
+
+    abstract class scoreboard
+    {
+        private int points, scores;
+        private String Team1, Team2;
+    }
+    public String 
+    }
+}
